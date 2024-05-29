d883daf1f878338526303518d04a5985c293a8d2
sebtul <youngdan1115@gmail.com>
2024-05-29 13:01:34 +0900

    증가버튼 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const 리턴값 = useState(0);
+  const count = 리턴값[0];
+  const setCount = 리턴값[1];
+
   return (
     <div>
-      <h1>0</h1>
-      <button onClick={() => console.log("클릭됨")}>증가</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((현재count) => 현재count + 1)}>
+        증가
+      </button>
     </div>
   );
 }
